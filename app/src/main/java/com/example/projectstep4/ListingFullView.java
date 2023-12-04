ea74a5f1e79ef53df5dbf973455ec57264bb0114
Liam Goel <liam.goel0678@gmail.com>
2023-12-03 20:17:09 -0800

    Update ListingFullView.java

--- app/src/main/java/com/example/projectstep4/ListingFullView.java
+++ app/src/main/java/com/example/projectstep4/ListingFullView.java
@@ -2,7 +2,14 @@ package com.example.projectstep4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListingFullView extends AppCompatActivity
 {
@@ -12,5 +19,22 @@ public class ListingFullView extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing_full_view);
+
+        Intent i = getIntent();
+        ListingProfile lp = i.getSerializableExtra("Listing Profile", ListingProfile.class);
+
+        ImageView img = findViewById(R.id.imageView3);
+        TextView primary = findViewById(R.id.textView25), pets = findViewById(R.id.textView26), general = findViewById(R.id.textView27);
+        String pmrStr, petStr, genStr;
+
+        pmrStr = "Postal Code: " + lp.postalCode + "\nOwner Name: " + lp.getfName();
+
+        petStr = "Pets Allowed: ";
+        ArrayList<String> als = (ArrayList<String>) lp.petsAllowed.keySet(); ArrayList<Integer> ali = (ArrayList<Integer>) lp.petsAllowed.values();
+    }
+
+    public void back (View v)
+    {
+        finish();
     }
 }
